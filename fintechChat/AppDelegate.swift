2a39a5ff653c1423a8b11f39ec0ceb15775cb541
Дмитрий Пятин <stepanet@gmail.com>
2019-02-07 02:12:41 +0300

    Initial Commit

---
+++ fintechChat/AppDelegate.swift
@@ -0,0 +1,78 @@
+//
+//  AppDelegate.swift
+//  fintechChat
+//
+//  Created by Jack Sp@rroW on 06/02/2019.
+//  Copyright © 2019 Jack Sp@rroW. All rights reserved.
+//
+
+import UIKit
+
+
+struct ShowLog {
+    static var show = true
+}
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    //переменная которая определяет, что приложение запускается первый раз для правильного отслеживания перехода по состояниям
+    var firstStart: Bool = true
+    
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        
+        //true - показать консоль
+        ShowLog.show = true
+        
+        if ShowLog.show {
+            print("Aplication state-> \(#function)")
+        }
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        if ShowLog.show {
+            print("Application moved from applicationDidBecomeActive to -> \(#function)")
+        }
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        if ShowLog.show {
+            print("Application moved from applicationWillResignActive to -> \(#function)")
+        }
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        if ShowLog.show {
+            print("Application moved from applicationDidEnterBackground to -> \(#function)")
+        }
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        
+        if ShowLog.show {
+            if firstStart {
+                print("Application moved from didFinishLaunchingWithOptions to -> \(#function)")
+                firstStart = false
+            } else {
+                print("Application moved from applicationWillEnterForeground to -> \(#function)")
+            }
+        }
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        
+        if ShowLog.show {
+            print(#function)
+        }
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
---
+++ fintechChat/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  fintechChat
+//
+//  Created by Jack Sp@rroW on 06/02/2019.
+//  Copyright © 2019 Jack Sp@rroW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.init(named: "backGrndColor")
+        
+        //есть сомнения, но без закрытия view эти методы не работают
+        self.viewWillDisappear(true)
+        self.viewDidDisappear(true)
+
+    }
+    
+    
+    
+    override func viewDidLayoutSubviews() {
+        if ShowLog.show {
+            print(#function)
+        }
+    }
+    
+    override func viewWillLayoutSubviews() {
+        if ShowLog.show {
+            print(#function)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if ShowLog.show {
+            print(#function)
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        if ShowLog.show {
+            print(#function)
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if ShowLog.show {
+            print(#function)
+        }
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        if ShowLog.show {
+            print(#function)
+        }
+    }
+    
+
+
+}
+
