e82d46737f4b68b0ae22677d42ad31e940f707f0
stepanet <35365880+stepanet@users.noreply.github.com>
2019-03-12 13:14:56 +0300

    Update ReadWriteDataToDisk.swift

--- fintechChat/Model/ReadWriteDataToDisk.swift
+++ fintechChat/Model/ReadWriteDataToDisk.swift
@@ -18,7 +18,163 @@ public class ReadWriteData {
     }
     
     
-    //начало класса
+    
+    
+    
+    class GCDDataManager {
+        var nameOfFile: String
+        var text: String
+        var selectedImage: UIImage
+        let queueGlobal = DispatchQueue.global()  //глобальная очередь
+        let queueMain = DispatchQueue.main        //главная очередь
+        let queue = DispatchQueue(label: "test thread") //последовательная очередь
+        let cuncurrentQueue = DispatchQueue(label: "com.app.queue",  qos: .background, attributes: .concurrent) //параллельная очередь
+        
+        init (nameOfFile: String = "", text: String = "", selectedImage: UIImage = UIImage(named: "placeholder-user.jpg")!){
+            self.nameOfFile = nameOfFile
+            self.text = text
+            self.selectedImage = selectedImage
+        }
+        
+        //считываем данные из текстовых файлов
+        func txtREadfile() -> String {
+            return self.queue.sync {
+                //print(Thread.current, "поток чтения файла")
+                if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+                    let fileURL = dir.appendingPathComponent(nameOfFile)
+                    do {
+                        return try String(contentsOf: fileURL, encoding: .utf8)
+                    }
+                    catch {/* обработчик ошибок */}
+                }
+                return "информация отсутствует"
+            }
+        }
+        
+        //забираем изображение из файла
+        func getImage() -> UIImage {
+
+           return self.queue.sync {
+                let fileManager = FileManager.default
+                let imagePath = (NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString).appendingPathComponent(nameOfFile)
+                if fileManager.fileExists(atPath: imagePath){
+                    return UIImage(contentsOfFile: imagePath)!
+                }else{
+                    return UIImage(named: "placeholder-user.jpg")!
+                }
+            }
+        }
+        
+        //пишем текст в файл
+    func txtWriteFile() {
+            
+            if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+                let fileURL = dir.appendingPathComponent(nameOfFile)
+                self.queue.sync {
+                
+                    do {
+                        try self.text.write(to: fileURL, atomically: false, encoding: .utf8)
+                    }
+                    catch {/* обработчик ошибок */}
+                }
+            }
+        }
+        
+        
+        //сохраняем изображение
+        func saveImage() {
+            
+                let fileManager = FileManager.default
+                let paths = (NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString).appendingPathComponent(nameOfFile)
+                print(paths)
+            self.queue.sync {
+                let imageData = selectedImage.jpegData(compressionQuality: 0.75)
+            
+                fileManager.createFile(atPath: paths as String, contents: imageData, attributes: nil)
+            }
+        }
+        
+    }
+    //конец класса GCDManager
+    
+    
+    
+    class OperationDataManager {
+        var nameOfFile: String
+        var text: String
+        var selectedImage: UIImage
+        let operationQueue = OperationQueue()   
+        
+        init (nameOfFile: String = "", text: String = "", selectedImage: UIImage = UIImage(named: "placeholder-user.jpg")!){
+            self.nameOfFile = nameOfFile
+            self.text = text
+            self.selectedImage = selectedImage
+        }
+        
+        //считываем данные из текстовых файлов
+        func txtREadfile() -> String {
+            return self.queue.sync {
+                //print(Thread.current, "поток чтения файла")
+                if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+                    let fileURL = dir.appendingPathComponent(nameOfFile)
+                    do {
+                        return try String(contentsOf: fileURL, encoding: .utf8)
+                    }
+                    catch {/* обработчик ошибок */}
+                }
+                return "информация отсутствует"
+            }
+        }
+        
+        //забираем изображение из файла
+        func getImage() -> UIImage {
+
+           return self.queue.sync {
+                let fileManager = FileManager.default
+                let imagePath = (NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString).appendingPathComponent(nameOfFile)
+                if fileManager.fileExists(atPath: imagePath){
+                    return UIImage(contentsOfFile: imagePath)!
+                }else{
+                    return UIImage(named: "placeholder-user.jpg")!
+                }
+            }
+        }
+        
+        //пишем текст в файл
+    func txtWriteFile() {
+            
+            if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+                let fileURL = dir.appendingPathComponent(nameOfFile)
+                self.queue.sync {
+                
+                    do {
+                        try self.text.write(to: fileURL, atomically: false, encoding: .utf8)
+                    }
+                    catch {/* обработчик ошибок */}
+                }
+            }
+        }
+        
+        
+        //сохраняем изображение
+        func saveImage() {
+            
+                let fileManager = FileManager.default
+                let paths = (NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString).appendingPathComponent(nameOfFile)
+                print(paths)
+            self.queue.sync {
+                let imageData = selectedImage.jpegData(compressionQuality: 0.75)
+            
+                fileManager.createFile(atPath: paths as String, contents: imageData, attributes: nil)
+            }
+        }
+        
+    }
+    //конец класса OperationManager
+            
+            
+    
+    //начало класса QueueChoiceTest
     
     class QueueChoiceTest {
         var nameOfFile: String
